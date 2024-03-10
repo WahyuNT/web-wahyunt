8e303f735a233042ea659425a51828d16a15f0ea
Wahyu NT <wahyuntx28@gmail.com>
2024-03-11 02:49:40 +0700

    Update background and card styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,6 +43,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       {/* BootstrapJS  */}
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossOrigin="anonymous" async />
+  
       <Component {...pageProps} />
 
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,23 +12,22 @@ export default function Home() {
     <>
       <div className="section-primary">
 
-        <div className="container pt-5">
+        <div className="container px-5 pt-5">
 
           <div className="d-flex justify-content-center gap-3">
-            <div className="col-3 ">
+            <div className="col-3  ">
 
               <div className="card card-glass w-100 p-5">
                 <div className=" d-flex justify-content-center align-items-center">
-                  <div className="avatar avatar-profile">
-                    <img src="https://avatars.githubusercontent.com/u/83133470?v=4" alt="" className="rounded-circle" />
-                  </div>
+                  <img src="https://avatars.githubusercontent.com/u/83133470?v=4" className="avatar-profile" alt="icon" />
 
                 </div>
-                <h5 className="text-white text-center">Wahyu NT</h5>
-                <hr className="" />
+                <h5 className="text-primary text-center mt-3 ">Wahyu NT</h5>
+                <p className=" text-center ">Full Stack Developer</p>
+                <hr className="mt-0" />
                 <div className="d-flex">
                   <div className="me-2">
-                    <span className="badge bg-label-info p-2">  <i className="fa-regular fa-envelope"></i></span>
+                    <button className="btn btn-icon btn-primary"><i className="fa-regular fa-envelope"></i></button>
                   </div>
                   <div className="d-flex flex-column">
                     <small className="text-white">2022</small>
@@ -41,7 +40,80 @@ export default function Home() {
 
               <div className="card card-glass w-100">
                 <div className="card-body">
-                  sadas
+                  <h5 className="text-primary"><b>About Me</b></h5>
+                  <p className="text-muted">Hello, I'm Wahyu Nusantara. I'm a fresh graduate from AMIKOM University Yogyakarta. With a background
+                    in UI/UX design, 3D Modeling, Game development, and Web development, I have a great interest in the
+                    world of programming. My expertise spans various languages and frameworks, including C#, JavaScript,
+                    PHP, Laravel, React.js, and Express.js. Additionally, I am proficient in managing data structure. </p>
+                  <h5 className="text-primary"><b>What I'm Doing</b></h5>
+                  <div className="d-flex justify-content-start ">
+
+                    <div className="col-4 pe-2">
+                      <div className="card">
+                        s
+                      </div>
+                    </div>
+                    <div className="col-4 ps-1 pe-1">
+                      <div className="card">
+                        s
+                      </div>
+                    </div>
+                    <div className="col-4 ps-2">
+                      <div className="card">
+                        s
+                      </div>
+                    </div>
+                  </div>
+                  <h5 className="text-primary mt-3"><b>Tech Skill</b></h5>
+                  <div className="d-flex justify-content-start flex-wrap">
+                    <li data-bs-toggle="tooltip" data-popup="tooltip-custom" data-bs-placement="top" className="avatar pull-up" aria-label="Darcey Nooner" data-bs-original-title="Darcey Nooner">
+                      <img className="rounded-circle" src="" alt="Avatar"/>
+                    </li>
+                    <div className="avatar avatar-skill me-2 mb-2 pull-up" data-popup="tooltip-custom" data-bs-placement="top" aria-label="Blender" data-bs-original-title="Blender">
+                      <img src="/img/icon/blender.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/express.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/figma.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/js.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/laravel.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/react.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/unity.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/vue.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/ue.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/corel.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/mongo.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/premiere.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/ae.png" className="rounded" alt="" />
+                    </div>
+                    <div className="avatar avatar-skill me-2 mb-2">
+                      <img src="/img/icon/ps.png" className="rounded" alt="" />
+                    </div>
+
+
+                  </div>
                 </div>
               </div>
             </div>
