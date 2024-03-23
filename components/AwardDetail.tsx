250d1f3f503890898a36114d557b1a63e9a31415
Wahyu NT <wahyuntx28@gmail.com>
2024-03-23 16:38:15 +0700

    Add Slider component and fetch images for AwardDetail page

--- components/AwardDetail.tsx
+++ components/AwardDetail.tsx
@@ -3,11 +3,26 @@ import axios from 'axios';
 import Link from 'next/link'
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Console } from 'console';
 
 export default function AwardDetail() {
+    var settings = {
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 0,
+
+    };
+
     const router = useRouter();
     const { slug } = router.query;
-    const [award, setAward] = useState({ title: '', desc: '', icon_company: '', cover: '', company: '', desc_company: '', date: '', slug: '' }); // Ganti array dengan objek tunggal
+    const [award, setAward] = useState({ _id: '', title: '', desc: '', icon_company: '', cover: '', company: '', desc_company: '', date: '', slug: '' }); // Ganti array dengan objek tunggal
+    const [image, setImage] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -15,7 +30,11 @@ export default function AwardDetail() {
                 if (slug) { // Pastikan slug tidak undefined
                     const responseAward = await axios.get(`https://portodb.wahyunt.me/api/award/detail/${slug}`);
                     setAward(responseAward.data.data);
-                    console.log(responseAward.data.data);
+                    const id = responseAward.data.data._id;
+                    if (id) {
+                        const responseImage = await axios.get(`https://portodb.wahyunt.me/api/award/image/list/${id}`);
+                        setImage(responseImage.data.data);
+                    }
                 }
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -64,17 +83,46 @@ export default function AwardDetail() {
                 </p>
             </div>
 
+            <div className=" slider-container px-4">
+                {image && image.length > 0 && (
+                    <Slider {...settings}>
+                        {image.map((img: { file_name: string; youtube?: string }) => {
+                            return (
+                                <div key={img.file_name}>
+                                    {img.youtube ? (
+                                        <div className="px-1">
+                                            <div className="bg-transparent card-glass p-2">
+                                                <div className="card bg-transparent p-1">
+                                                    <iframe title="video" width="auto" height="218px" src={img.youtube}></iframe>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <div className="px-1">
+                                            <div className="bg-transparent card-glass p-2">
+                                                <div className="card bg-transparent p-1">
+                                                    <img src={img.file_name} className="detail-porto-image" alt="" />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </Slider>
+                )}
+
+            </div>
 
 
-
-            <div className="d-flex">
+            {/* <div className="d-flex">
                 <div className="col-12 ">
                     <div className="card  bg-transparent ">
                         <img src={award.cover} className='w-100 h-100 award-image' alt="" />
                     </div>
                 </div>
-                
-            </div>
+
+            </div> */}
 
 
 
