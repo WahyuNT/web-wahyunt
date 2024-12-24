4ca52716bf5148522d2a2cc780dc882e8bfd5131
Wahyu NT <wahyuntx28@gmail.com>
2024-12-24 18:15:12 +0700

    Add CV page and integrate Sidebar component

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,61 +1,69 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect } from "react";
 
 export default function Sidebar() {
-    const [isActive, setIsActive] = useState(false);
-    const [location, setLocation] = useState('');
+  const [isActive, setIsActive] = useState(false);
+  const [location, setLocation] = useState("");
 
-    useEffect(() => {
-        // Periksa apakah URL saat ini sama dengan href
-        const currentLocation = window.location.pathname;
-        setLocation(currentLocation);
+  useEffect(() => {
+    // Periksa apakah URL saat ini sama dengan href
+    const currentLocation = window.location.pathname;
+    setLocation(currentLocation);
+  }, []);
 
-
-    }, []);
-
-    return (
-        <div >
-
-            <div className="d-flex align-items-start justify-content-center flex-wrap gap-3 ">
-                <div className="col d-flex justify-content-center align-items-start mb-1">
-                    <Link href="/" className='text-decoration-none'>
-                        <button className={`btn p-3 btn-icon d-flex flex-column ${location === '/' ? 'text-utama' : 'text-white'}`}>
-                            <i className="fa-solid fa-user   "></i>
-                            <small>
-                                <small>
-                                    <small className=''>About</small>
-                                </small>
-                            </small>
-                        </button>
-                    </Link>
-                </div>
-                <div className="col d-flex justify-content-center mb-1">
-                    <Link href="/resume" className='text-decoration-none'>
-                        <button className={`btn p-3 btn-icon d-flex flex-column ${location === '/resume' ? 'text-utama' : 'text-white'}`}>
-                            <i className="fa-solid fa-briefcase "></i>
-                            <small>
-                                <small>
-                                    <small className=''>Resume</small>
-                                </small>
-                            </small>
-                        </button>
-                    </Link>
-                </div>
-                <div className="col d-flex justify-content-center mb-1">
-                    <Link href="/portofolio" className='text-decoration-none'>
-                        <button className={`btn p-3 btn-icon d-flex flex-column ${location === '/portofolio' ? 'text-utama' : 'text-white'}`}>
-                            <i className="fa-solid fa-images "></i>
-                            <small>
-                                <small>
-                                    <small className=''>Portofolio</small>
-                                </small>
-                            </small>
-                        </button>
-                    </Link>
-                </div>
-                {/* <div className="col d-flex justify-content-center mb-1">
+  return (
+    <div>
+      <div className="d-flex align-items-start justify-content-center flex-wrap gap-3 ">
+        <div className="col d-flex justify-content-center align-items-start mb-1">
+          <Link href="/" className="text-decoration-none">
+            <button
+              className={`btn p-3 btn-icon d-flex flex-column ${
+                location === "/" ? "text-utama" : "text-white"
+              }`}
+            >
+              <i className="fa-solid fa-user   "></i>
+              <small>
+                <small>
+                  <small className="">About</small>
+                </small>
+              </small>
+            </button>
+          </Link>
+        </div>
+        <div className="col d-flex justify-content-center mb-1">
+          <Link href="/resume" className="text-decoration-none">
+            <button
+              className={`btn p-3 btn-icon d-flex flex-column ${
+                location === "/resume" ? "text-utama" : "text-white"
+              }`}
+            >
+              <i className="fa-solid fa-briefcase "></i>
+              <small>
+                <small>
+                  <small className="">Resume</small>
+                </small>
+              </small>
+            </button>
+          </Link>
+        </div>
+        <div className="col d-flex justify-content-center mb-1">
+          <Link href="/portofolio" className="text-decoration-none">
+            <button
+              className={`btn p-3 btn-icon d-flex flex-column ${
+                location === "/portofolio" ? "text-utama" : "text-white"
+              }`}
+            >
+              <i className="fa-solid fa-images "></i>
+              <small>
+                <small>
+                  <small className="">Portofolio</small>
+                </small>
+              </small>
+            </button>
+          </Link>
+        </div>
+        {/* <div className="col d-flex justify-content-center mb-1">
                     <Link href="/blog" className='text-decoration-none'>
                         <button className='btn p-3 btn-icon d-flex flex-column' >
                             <i className="fa-solid fa-newspaper text-white ${isActive ? 'text-utama' : ''}"></i>
@@ -67,32 +75,55 @@ export default function Sidebar() {
                         </button>
                     </Link>
                 </div> */}
-                <div className="col d-flex justify-content-center mb-1">
-                    <Link href="/certificate" className='text-decoration-none'>
-                        <button className={`btn p-3 btn-icon d-flex flex-column ${location === '/certificate' ? 'text-utama' : 'text-white'}`} >
-                            <i className="fa-solid fa-file-invoice"></i>
-                            <small>
-                                <small>
-                                    <small className=''>Certificate</small>
-                                </small>
-                            </small>
-                        </button>
-                    </Link>
-                </div>
-                <div className="col d-flex justify-content-center mb-1">
-                    <Link href="/award" className='text-decoration-none'>
-                        <button className={`btn p-3 btn-icon d-flex flex-column ${location === '/award' ? 'text-utama' : 'text-white'}`} >
-                            <i className="fa-solid fa-award "></i>
-                            <small>
-                                <small>
-                                    <small className=''>Award</small>
-                                </small>
-                            </small>
-                        </button>
-                    </Link>
-                </div>
-
-            </div>
+        <div className="col d-flex justify-content-center mb-1">
+          <Link href="/certificate" className="text-decoration-none">
+            <button
+              className={`btn p-3 btn-icon d-flex flex-column ${
+                location === "/certificate" ? "text-utama" : "text-white"
+              }`}
+            >
+              <i className="fa-solid fa-file-contract"></i>
+              <small>
+                <small>
+                  <small className="">Certificate</small>
+                </small>
+              </small>
+            </button>
+          </Link>
         </div>
-    )
+        <div className="col d-flex justify-content-center mb-1">
+          <Link href="/award" className="text-decoration-none">
+            <button
+              className={`btn p-3 btn-icon d-flex flex-column ${
+                location === "/award" ? "text-utama" : "text-white"
+              }`}
+            >
+              <i className="fa-solid fa-award "></i>
+              <small>
+                <small>
+                  <small className="">Award</small>
+                </small>
+              </small>
+            </button>
+          </Link>
+        </div>
+        <div className="col d-flex justify-content-center mb-1">
+          <Link href="/cv" className="text-decoration-none">
+            <button
+              className={`btn p-3 btn-icon d-flex flex-column ${
+                location === "/cv" ? "text-utama" : "text-white"
+              }`}
+            >
+              <i className="fa-solid fa-file-invoice "></i>
+              <small>
+                <small>
+                  <small className="">CV</small>
+                </small>
+              </small>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
