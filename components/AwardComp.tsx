28dbd97bbccb825c9d2aa2a5d37dc3618bda3497
Wahyu NT <wahyuntx28@gmail.com>
2024-12-24 17:58:20 +0700

    Add loading state to CertificateComp and handle data fetching

--- components/AwardComp.tsx
+++ components/AwardComp.tsx
@@ -1,82 +1,112 @@
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 export default function AwardComp() {
-    const [award, setAward] = useState([]);
+  const [award, setAward] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const responseAward = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/award`);
-                setAward(responseAward.data.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const responseAward = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/award`
+        );
+        setAward(responseAward.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
+    fetchData();
+  }, []);
 
-
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return (
-        <div><div className="card-body  px-1" style={{ maxHeight: "100%" }}>
-
-            <div className="d-flex">
-
-            </div>
-            <h4 className="text-utama mt-2 text-center"><b>Award</b></h4>
-            <div className="d-flex  flex-wrap">
-                {award.map((item: { title: string, cover: string, desc: string, icon_company: string, company: string, desc_company: string, date: string, slug: string }) => {
-                    return (
-                        <div className="col-lg-4 col-12 mb-1 p-2">
-                            <div className=" bg-transparent card h-100 card-glass card-porto pb-2">
-                                <div className="card bg-transparent card-body p-0 card-banner flex-column">
-                                    <div className="card bg-transparent">
-                                        <img src={item.cover} className="cover-award mb-2" alt="" />
-                                    </div>
-                                    <div className="d-flex justify-content-center ">
-                                        <div className="card bg-transparent ">
-                                            <img src={item.icon_company} className="cover-ava-award " alt="" />
-                                        </div>
-                                    </div>
-                                    <div className="div ">
-
-                                        <div className="px-3 text-center">
-                                            <br />
-                                            <p className='text-white text-utama fw-bold mt-1 mb-0'>{item.title}</p>
-                                            <p className='text-second mb-1 mt-1' style={{ fontSize: "13px" }}>
-                                                {item.desc}
-                                            </p>
-                                        </div>
-                                    </div>
-
-
-                                </div>
-                                <div className="card-footer p-0" >
-
-                                    <div className="px-3  ">
-
-                                        <hr className=' mb-0  mt-1 border-2 color-border' />
-                                    </div>
-                                    <Link href={`/award/detail/${item.slug}` } className='text-decoration-none'>
-
-                                        <div className=' text-link'>
-                                            <div className="d-flex justify-content-between align-items-center mt-2  px-3 pb-1 ">
-                                                <small className=' fw-bold  '>Read More</small>
-                                                <i className="fa-solid fa-caret-right ms-1 fa-sm"></i>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
+  return (
+    <div>
+      <div className="card-body  px-1" style={{ maxHeight: "100%" }}>
+        <div className="d-flex"></div>
+        <h4 className="text-utama mt-2 text-center">
+          <b>Award</b>
+        </h4>
+        {isLoading ? (
+          <div className="d-flex justify-content-center">
+            <span className="loader mt-5 "></span>
+          </div>
+        ) : (
+          <div className="d-flex  flex-wrap">
+            {award.map(
+              (item: {
+                title: string;
+                cover: string;
+                desc: string;
+                icon_company: string;
+                company: string;
+                desc_company: string;
+                date: string;
+                slug: string;
+              }) => {
+                return (
+                  <div className="col-lg-4 col-12 mb-1 p-2">
+                    <div className=" bg-transparent card h-100 card-glass card-porto pb-2">
+                      <div className="card bg-transparent card-body p-0 card-banner flex-column">
+                        <div className="card bg-transparent">
+                          <img
+                            src={item.cover}
+                            className="cover-award mb-2"
+                            alt=""
+                          />
                         </div>
-                    )
-                })}
-            </div>
-
-        </div></div>
-    )
+                        <div className="d-flex justify-content-center ">
+                          <div className="card bg-transparent ">
+                            <img
+                              src={item.icon_company}
+                              className="cover-ava-award "
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                        <div className="div ">
+                          <div className="px-3 text-center">
+                            <br />
+                            <p className="text-white text-utama fw-bold mt-1 mb-0">
+                              {item.title}
+                            </p>
+                            <p
+                              className="text-second mb-1 mt-1"
+                              style={{ fontSize: "13px" }}
+                            >
+                              {item.desc}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="card-footer p-0">
+                        <div className="px-3  ">
+                          <hr className=" mb-0  mt-1 border-2 color-border" />
+                        </div>
+                        <Link
+                          href={`/award/detail/${item.slug}`}
+                          className="text-decoration-none"
+                        >
+                          <div className=" text-link">
+                            <div className="d-flex justify-content-between align-items-center mt-2  px-3 pb-1 ">
+                              <small className=" fw-bold  ">Read More</small>
+                              <i className="fa-solid fa-caret-right ms-1 fa-sm"></i>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- components/CertificateComp.tsx
+++ components/CertificateComp.tsx
@@ -1,75 +1,103 @@
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 export default function CertificateComp() {
-    const [certificate, setCertificate] = useState([]);
+  const [certificate, setCertificate] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const responseCertificate = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/certificate`);
-                setCertificate(responseCertificate.data.data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const responseCertificate = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/certificate`
+        );
+        setCertificate(responseCertificate.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-
-    return (
-        <div><div className="card-body  px-1" style={{ maxHeight: "100%" }}>
-
-            <div className="d-flex">
-
-            </div>
-            <h4 className="text-utama mt-2 text-center"><b>Certificate</b></h4>
-            <div className="d-flex  flex-wrap">
-
-                {certificate.map((item: { slug: string, image: string, title: string, company: string, year: string, credential: string, }) => {
-                    return (
-                        <div className="col-lg-4 col-12 mb-1 p-2">
-                            <div className=" bg-transparent card h-100 card-glass card-porto pb-2">
-                                <div className="card bg-transparent  card-banner flex-column">
-                                    <div className="card bg-transparent pt-3 px-3">
-                                        <img src={item.image} className="cover-certificate mb-2" alt="" />
-                                    </div>
-                                    <div className="px-3">
-                                        <p className='text-utama fw-bold mt-0 mb-0'>{item.title} </p>
-                                        <div className="d-flex align-items-center">
-
-                                            <div className="col">
-                                                <p className='text-second mb-1 mt-1' style={{ fontSize: "13px" }}>
-                                                    <i className="fa-solid fa-building me-1"></i>{item.company} <br />
-                                                    <i className="fa-solid fa-calendar-days me-1"></i>{item.year} <br />
-                                                    {item.credential && (
-                                                        <React.Fragment>
-                                                            <i className="fa-solid fa-hashtag me-1"></i>{item.credential}
-                                                        </React.Fragment>
-                                                    )}
-                                                </p>
-                                            </div>
-                                            <div className="div">
-                                                <Link href={`/certificate/detail/${item.slug}`}>
-                                                    <button className='btn btn-icon btn-open-certificate  p-2'>
-                                                        <i className="fa-solid  fa-arrow-right-from-bracket  fa"></i>
-                                                    </button>
-                                                </Link>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                    <div className="d-flex justify-content-start align-items-center mt-2">
-                                    </div>
-                                </div>
-                            </div>
+  return (
+    <div>
+      <div className="card-body  px-1" style={{ maxHeight: "100%" }}>
+        <div className="d-flex"></div>
+        <h4 className="text-utama mt-2 text-center">
+          <b>Certificate</b>
+        </h4>
+        {isLoading ? (
+          <div className="d-flex justify-content-center">
+            <span className="loader mt-5 "></span>
+          </div>
+        ) : (
+          <div className="d-flex  flex-wrap">
+            {certificate.map(
+              (item: {
+                slug: string;
+                image: string;
+                title: string;
+                company: string;
+                year: string;
+                credential: string;
+              }) => {
+                return (
+                  <div className="col-lg-4 col-12 mb-1 p-2">
+                    <div className=" bg-transparent card h-100 card-glass card-porto pb-2">
+                      <div className="card bg-transparent  card-banner flex-column">
+                        <div className="card bg-transparent pt-3 px-3">
+                          <img
+                            src={item.image}
+                            className="cover-certificate mb-2"
+                            alt=""
+                          />
                         </div>
-                    )
-                })}
-            </div>
-
-        </div></div>
-    )
+                        <div className="px-3">
+                          <p className="text-utama fw-bold mt-0 mb-0">
+                            {item.title}{" "}
+                          </p>
+                          <div className="d-flex align-items-center">
+                            <div className="col">
+                              <p
+                                className="text-second mb-1 mt-1"
+                                style={{ fontSize: "13px" }}
+                              >
+                                <i className="fa-solid fa-building me-1"></i>
+                                {item.company} <br />
+                                <i className="fa-solid fa-calendar-days me-1"></i>
+                                {item.year} <br />
+                                {item.credential && (
+                                  <React.Fragment>
+                                    <i className="fa-solid fa-hashtag me-1"></i>
+                                    {item.credential}
+                                  </React.Fragment>
+                                )}
+                              </p>
+                            </div>
+                            <div className="div">
+                              <Link href={`/certificate/detail/${item.slug}`}>
+                                <button className="btn btn-icon btn-open-certificate  p-2">
+                                  <i className="fa-solid  fa-arrow-right-from-bracket  fa"></i>
+                                </button>
+                              </Link>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="d-flex justify-content-start align-items-center mt-2"></div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
